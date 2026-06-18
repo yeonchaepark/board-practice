e0110934de8169a30f9c8623e10fc03c2b567fce
yeonchaepark <check4732@naver.com>
2026-06-18 14:10:13 +0900

    feat: Board 조회수(ViewCount) 추가

--- Board.java
+++ Board.java
@@ -4,3 +4,7 @@ public class Board {
     private String author;
     // getter/setter 생략
 }
+    // feature/A: 조회수 기능 추가
+    private int viewCount = 0;
+    public int  getViewCount()         { return viewCount; }
+    public void incrementViewCount()   { this.viewCount++; }
