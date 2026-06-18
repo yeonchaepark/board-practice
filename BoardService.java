c1deb226074128abca47b148b09daec86ccd7262
yeonchaepark <127640204+yeonchaepark@users.noreply.github.com>
2026-06-18 16:22:52 +0900

    Revert "feat: findAll 로그 추가(A버전)"

--- BoardService.java
+++ BoardService.java
@@ -1,8 +1,7 @@
 import java.util.*;
 public class BoardService {
     private List<Board> boards = new ArrayList<>();
-    public List<Board> findAll() { System.out.println("[A버전] 전체 목록 조회");
-        return boards; }
+    public List<Board> findAll() { return boards; }
     public Board       findById(int id) {
         return boards.stream()
                      .filter(b -> b.getId() == id)
