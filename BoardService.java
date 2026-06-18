24b632f6de6adafa02655bd3e35d8118340b2baf
yeonchaepark <check4732@naver.com>
2026-06-18 16:11:29 +0900

    feat: findAll 로그 추가(A버전)

--- BoardService.java
+++ BoardService.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class BoardService {
     private List<Board> boards = new ArrayList<>();
-    public List<Board> findAll() { return boards; }
+    public List<Board> findAll() { System.out.println("[A버전] 전체 목록 조회");
+        return boards; }
     public Board       findById(int id) {
         return boards.stream()
                      .filter(b -> b.getId() == id)
